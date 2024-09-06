62912ceb02b1adfebbdd4536392fc7d24e37a9a2
jeff25700 <jeffersonvitor006@gmail.com>
2024-09-05 21:28:43 -0300

    Update app.js

--- tavernaDoHistoriador/app.js
+++ tavernaDoHistoriador/app.js
@@ -1,1 +1,26 @@
-console.log(dados);
+function pesquisar() {
+
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    console.log(section); // Loga a seção no console para verificação
+
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+
+        // Concatena o HTML de cada item de resultado à string 'resultados'
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href=${dado.link} target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta"> ${dado.descricao} </p>
+            <a href='https://pt.wikipedia.org/wiki/Primeira_Guerra_Mundial' target="_blank">Mais informações</a>
+        </div>`;
+    }
+
+    // Atribui o HTML completo da lista de resultados à seção
+    section.innerHTML = resultados;
+}
