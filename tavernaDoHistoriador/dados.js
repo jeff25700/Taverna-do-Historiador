ddffd16a7cd00dd43a62d47b3d53d4f5bd63cf08
jeff25700 <jeffersonvitor006@gmail.com>
2024-09-07 15:02:08 -0300

    Update dados.js

--- tavernaDoHistoriador/dados.js
+++ tavernaDoHistoriador/dados.js
@@ -6,17 +6,17 @@ let dados = [
         tags: "grande guerra conflito armado"
     },
 
-   {
+    {
         titulo: 'Unificação da China:',
         descricao: 'As Guerras de unificação da China foram uma série de campanhas militares ocorridas no final do século III a.C.entre o Estado de Qin e seus rivais: Han, Zhao, Yan, Wei, Chu e Qi.Por volta do ano 221 a.C.o território chinês estava unificado e foi lançada uma campanha de conquista sobre o sul do rio Yangtze.',
-        link:'https://pt.wikipedia.org/wiki/Guerras_de_unificação_da_China',
+        link: 'https://pt.wikipedia.org/wiki/Guerras_de_unificação_da_China',
         tags: "qin shin huang asia"
-   },
+    },
 
-   {
+    {
         titulo: ' Guerra do Paraguai:',
         descricao: 'A Guerra do Paraguai foi o maior conflito armado internacional ocorrido na América Latina. Foi travada entre o Paraguai e a Tríplice Aliança, composta pelo Império do Brasil, Argentina e Uruguai. Ela se estendeu de dezembro de 1864 a março de 1870.',
         link: 'https://pt.wikipedia.org/wiki/Guerra_do_Paraguai',
-        tags: "acre marques de olinda  Marquês de Olinda"
+        tags: "acre marques de olinda  Marquês de Olinda america do sul"
     }
 ];
