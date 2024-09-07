be420d50d522d65ab3c34c9721ebc09f330c14b9
jeff25700 <jeffersonvitor006@gmail.com>
2024-09-07 01:04:47 -0300

    Update dados.js

--- tavernaDoHistoriador/dados.js
+++ tavernaDoHistoriador/dados.js
@@ -2,18 +2,21 @@ let dados = [
     {
         titulo: ' A Primeira Guerra Mundial:',
         descricao: 'A Primeira Guerra Mundial foi um conflito bélico global centrado na Europa, que começou em 28 de julho de 1914 e durou até 11 de novembro de 1918.',
-        link: 'https://pt.wikipedia.org/wiki/Primeira_Guerra_Mundial'
+        link: 'https://pt.wikipedia.org/wiki/Primeira_Guerra_Mundial',
+        tags: "grande guerra conflito armado"
     },
 
    {
         titulo: 'Unificação da China:',
         descricao: 'As Guerras de unificação da China foram uma série de campanhas militares ocorridas no final do século III a.C.entre o Estado de Qin e seus rivais: Han, Zhao, Yan, Wei, Chu e Qi.Por volta do ano 221 a.C.o território chinês estava unificado e foi lançada uma campanha de conquista sobre o sul do rio Yangtze.',
-        link:'https://pt.wikipedia.org/wiki/Guerras_de_unificação_da_China'
+        link:'https://pt.wikipedia.org/wiki/Guerras_de_unificação_da_China',
+        tags: "qin shin huang asia"
    },
 
    {
         titulo: ' Guerra do Paraguai:',
         descricao: 'A Guerra do Paraguai foi o maior conflito armado internacional ocorrido na América Latina. Foi travada entre o Paraguai e a Tríplice Aliança, composta pelo Império do Brasil, Argentina e Uruguai. Ela se estendeu de dezembro de 1864 a março de 1870.',
-        link: 'https://pt.wikipedia.org/wiki/Guerra_do_Paraguai'
+        link: 'https://pt.wikipedia.org/wiki/Guerra_do_Paraguai',
+        tags: "acre marques de olinda  Marquês de Olinda"
     }
 ];
