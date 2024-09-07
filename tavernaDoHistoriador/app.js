5e97630e52150e6886ab759cbfd9f75eceefd464
jeff25700 <jeffersonvitor006@gmail.com>
2024-09-07 01:05:15 -0300

    Update app.js

--- tavernaDoHistoriador/app.js
+++ tavernaDoHistoriador/app.js
@@ -4,14 +4,34 @@ function pesquisar() {
     let section = document.getElementById("resultados-pesquisa");
     console.log(section); // Loga a seção no console para verificação
 
+    let buscarHistoria = document.getElementById("buscarHistoria").value
+
+    //se buscarHistoria for uma string sem nada
+    if (buscarHistoria == "") {
+        section.innerHTML = ("<p> pesquisa não encontrada </p>")
+        return
+    }
+
+    buscarHistoria = buscarHistoria.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados formatados
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
 
-        // Concatena o HTML de cada item de resultado à string 'resultados'
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLocaleLowerCase()
+
+        // se titulo includes buscarHistoria
+        if (titulo.includes(buscarHistoria) || descricao.includes(buscarHistoria) || tags.includes(buscarHistoria)) {
+
+            // Concatena o HTML de cada item de resultado à string 'resultados'
+            resultados += `
         <div class="item-resultado">
             <h2>
                 <a href=${dado.link} target="_blank">${dado.titulo}</a>
@@ -19,6 +39,10 @@ function pesquisar() {
             <p class="descricao-meta"> ${dado.descricao} </p>
             <a href='https://pt.wikipedia.org/wiki/Primeira_Guerra_Mundial' target="_blank">Mais informações</a>
         </div>`;
+        }
+        // faça isso...
+        console.log(dado.titulo.includes(buscarHistoria));
+
     }
 
     // Atribui o HTML completo da lista de resultados à seção
